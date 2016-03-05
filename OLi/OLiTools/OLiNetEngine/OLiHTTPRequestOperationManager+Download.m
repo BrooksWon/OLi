781b013271cccdfa297643c01b989865eec0b1bd
BrooksWon <jianyu996@163.com>
2016-03-05 16:15:04 +0800

    修复解压缩bug

--- OLi/OLiTools/OLiNetEngine/OLiHTTPRequestOperationManager+Download.m
+++ OLi/OLiTools/OLiNetEngine/OLiHTTPRequestOperationManager+Download.m
@@ -33,7 +33,7 @@ static NSOperationQueue* OLiHTTPRequestOperationDownloadQueue() {
     NSOperationQueue *downloadQueue = OLiHTTPRequestOperationDownloadQueue();
     
     // 1. 建立请求
-//    NSURLRequest *request = [_httpClient requestWithMethod:@"GET" path:@"download/Objective-C2.0.zip" parameters:nil];
+    //    NSURLRequest *request = [_httpClient requestWithMethod:@"GET" path:@"download/Objective-C2.0.zip" parameters:nil];
     NSMutableURLRequest *request = [self.requestSerializer requestWithMethod:method
                                                                    URLString:[[NSURL URLWithString:URLString relativeToURL:self.baseURL] absoluteString]
                                                                   parameters:parameters
@@ -48,8 +48,8 @@ static NSOperationQueue* OLiHTTPRequestOperationDownloadQueue() {
     
     AFHTTPRequestOperation *op = [self HTTPRequestOperationWithRequest:request success:success failure:failure];
     
-//    // 2. 操作
-//    AFHTTPRequestOperation *op = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+    // 2. 操作
+    //    AFHTTPRequestOperation *op = [[AFHTTPRequestOperation alloc] initWithRequest:request];
     
     // 下载
     // 指定文件保存路径，将文件保存在沙盒中
@@ -60,24 +60,24 @@ static NSOperationQueue* OLiHTTPRequestOperationDownloadQueue() {
     
     // 设置下载进程块代码
     /*
-     21      bytesRead                      当前一次读取的字节数(100k)
-     22      totalBytesRead                 已经下载的字节数(4.9M）
-     23      totalBytesExpectedToRead       文件总大小(5M)
-     24      */
+     bytesRead                      当前一次读取的字节数(100k)
+     totalBytesRead                 已经下载的字节数(4.9M）
+     totalBytesExpectedToRead       文件总大小(5M)
+     */
     [op setDownloadProgressBlock:^(NSUInteger bytesRead, long long totalBytesRead, long long totalBytesExpectedToRead) {
         // 设置进度条的百分比
-//        CGFloat precent = (CGFloat)totalBytesRead / totalBytesExpectedToRead;
-//        _progressView.progress = precent;
+        //        CGFloat precent = (CGFloat)totalBytesRead / totalBytesExpectedToRead;
+        //        _progressView.progress = precent;
         downloadProgressBlock(bytesRead, totalBytesRead, totalBytesExpectedToRead);
     }];
     // 设置下载完成操作
     [op setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responSEObject) {
         // 下载完成之后，解压缩文件
         /*
-         39          参数1:要解结压缩的文件名及路径 path - > download.zip
-         40          参数2:要解压缩到的位置，目录    - > document目录
-         41          */
-        [SSZipArchive unzipFileAtPath:path toDestination:docs[0]];
+         参数1:要解结压缩的文件名及路径 path - > download.zip
+         参数2:要解压缩到的位置，目录    - > document目录
+         */
+//                [SSZipArchive unzipFileAtPath:path toDestination:docs[0]];
         // 解压缩之后，将原始的压缩包删除
         // NSFileManager专门用于文件管理操作，可以删除，复制，移动文件等操作
         // 也可以检查文件是否存在
@@ -93,6 +93,8 @@ static NSOperationQueue* OLiHTTPRequestOperationDownloadQueue() {
     
     // 启动下载
     [downloadQueue addOperation:op];
+    
+    return op;
 }
 
 
